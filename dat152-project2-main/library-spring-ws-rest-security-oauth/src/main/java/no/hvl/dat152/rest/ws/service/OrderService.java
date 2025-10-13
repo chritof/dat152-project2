6dcf736a63734ce890df932e7bf2f16be26fd56c
christoffertoft13 <chrtof13@gmail.com>
2025-10-13 14:12:57 +0200

    first commit

---
+++ dat152-project2-main/library-spring-ws-rest-security-oauth/src/main/java/no/hvl/dat152/rest/ws/service/OrderService.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package no.hvl.dat152.rest.ws.service;
+
+import java.util.List;
+
+import java.time.LocalDate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+import no.hvl.dat152.rest.ws.exceptions.OrderNotFoundException;
+import no.hvl.dat152.rest.ws.exceptions.UnauthorizedOrderActionException;
+import no.hvl.dat152.rest.ws.model.Order;
+import no.hvl.dat152.rest.ws.repository.OrderRepository;
+import no.hvl.dat152.rest.ws.security.UserDetailsImpl;
+
+/**
+ * @author tdoy
+ */
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	// TODO copy your solutions from previous tasks!
+	
+
+}
